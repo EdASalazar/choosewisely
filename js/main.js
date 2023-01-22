48b827737a834754204632fed0a5647a00d5a92b
EdASalazar <edasalazar123@gmail.com>
2023-01-22 14:49:17 -0800

    building the came const.

--- js/main.js
+++ js/main.js
@@ -14,43 +14,45 @@ const msgEl = document.getElementById('message');
 
 /*----- classes -----*/
 class ChooseWiselyGame {
-  constructor(boardElement) {
+  constructor(boardElement, messageElement) {
     this.boardElement = boardElement;
+    this.messageElement = messageElement;
   }
 }
 
-class Square {
-  constructor(domElement) {
-    this.domElement = domElement;
-    this.value = null;
-  }
+// class Square {
+//   constructor(domElement) {
+//     this.domElement = domElement;
+//     this.value = null;
+//   }
 
 /*----- functions -----*/
-init();
+// init();
 
-function init() {
-  board = [];
-  for (let rowIdx = 0; rowIdx < BOARD_ROWS; rowIdx++) {
-    board[rowIdx] = [];
-    for (let colIdx = 0; colIdx < BOARD_COLS; colIdx++) {
-      board[rowIdx].push({
-        isMine: false,  // TBD later
-        isRevealed: false,
-        isFlagged: false,
-        adjMineCount: null,  // Compute after mines are determined
-        rowIdx,
-        colIdx,
-      });
-    }
-  }
+// function init() {
+//   board = [];
+//   for (let rowIdx = 0; rowIdx < BOARD_ROWS; rowIdx++) {
+//     board[rowIdx] = [];
+//     for (let colIdx = 0; colIdx < BOARD_COLS; colIdx++) {
+//       board[rowIdx].push({
+//         isMine: false,  // TBD later
+//         isRevealed: false,
+//         isFlagged: false,
+//         adjMineCount: null,  // Compute after mines are determined
+//         rowIdx,
+//         colIdx,
+//       });
+//     }
+//   }
   // TODO: Iterate over cells to plant mines
   // TODO: Iterate over cells to compute adjMineCount
-}
+// }
 
 
 initialize();
 
 function initialize() {
   game = new ChooseWiselyGame(boardEl);
-  
+
+
 }
