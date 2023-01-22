269b2d24d3630debcb46c410f13ce6ac5b6d9ceb
EdASalazar <edasalazar123@gmail.com>
2023-01-22 14:12:43 -0800

    updated javascript witht some of the code for rows and columns

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+/*----- constants -----*/
+const BOARD_ROWS = 8;
+const BOARD_COLS = 8;
+
+
+
 /*----- app's state (variables) -----*/
 let game;
 
@@ -8,6 +14,32 @@ const msgEl = document.getElementById('message');
 /*----- classes -----*/
 
 /*----- functions -----*/
+
+init();
+
+function init() {
+  board = [];
+  for (let rowIdx = 0; rowIdx < BOARD_ROWS; rowIdx++) {
+    board[rowIdx] = [];
+    for (let colIdx = 0; colIdx < BOARD_COLS; colIdx++) {
+      board[rowIdx].push({
+        isMine: false,  // TBD later
+        isRevealed: false,
+        isFlagged: false,
+        adjMineCount: null,  // Compute after mines are determined
+        rowIdx,
+        colIdx
+        board[rowIdx].push({
+          isMine: Math.random() < (MINE_PCT / 100),
+          isRevealed: false,
+      });
+    }
+  }
+  // TODO: Iterate over cells to plant mines
+  /
+
+
+
 initialize();
 
 function initialize() {
